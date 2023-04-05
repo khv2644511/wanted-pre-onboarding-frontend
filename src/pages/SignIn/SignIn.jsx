d3982a6d75cbc0b2273c3f64a8c06eeef67dec08
HYEBIN KIM <khv2644511@naver.com>
2023-04-06 03:58:25 +0900

    Design: sign in 컴포넌트 마크업 및 UI생성 (#4)
    * init: styled-componets 셋팅
    
    * feat: GlobalStyle 전역 변수, 폰트 추가
    
    * feat: Button 컴포넌트 마크업 및 UI
    
    * feat: SignIn 컴포넌트 마크업 및 UI

--- src/pages/SignIn/SignIn.jsx
+++ src/pages/SignIn/SignIn.jsx
@@ -1,5 +1,21 @@
+import Button from "../../components/common/Button/Button";
 import React from "react";
+import { S } from "./SignInStyle";
 
 export default function SignIn() {
-  return <div>SignIn</div>;
+  return (
+    <main>
+      <h1 className="ir-hidden">로그인페이지</h1>
+      <S.Container>
+        <S.Title>Login</S.Title>
+        <S.FormContainer>
+          <label>Email</label>
+          <input type="email" placeholder="email" width="auto" />
+          <label>Password</label>
+          <input type="password" placeholder="password" />
+          <Button theme={""}>Sign In</Button>
+        </S.FormContainer>
+      </S.Container>
+    </main>
+  );
 }
---
+++ src/pages/SignIn/SignInStyle.jsx
@@ -0,0 +1,48 @@
+import styled from "styled-components";
+
+const Container = styled.article`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 400px;
+  height: 320px;
+  background-color: var(--color-type-02);
+  border: 2px solid var(--color-type-03);
+  border-radius: 10px;
+  text-align: center;
+`;
+
+const Title = styled.h2`
+  font-family: "Noto Sans KR", sans-serif;
+  font-size: 2rem;
+  width: 100%;
+  padding: 10px;
+  height: 70px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 10px 10px 0 0;
+  color: var(--color-type-01);
+`;
+
+const FormContainer = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-top: 10px;
+  label {
+    padding-bottom: 5px;
+  }
+  input {
+    width: 330px;
+    height: 40px;
+    padding-left: 10px;
+    margin-bottom: 25px;
+    border-radius: 5px;
+    background-color: var(--color-type-04);
+    font-size: 1rem;
+  }
+`;
+
+export const S = { Container, Title, FormContainer };
