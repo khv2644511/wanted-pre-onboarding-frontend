623802a0a857f73934e1cd962e1e8ed9b47f13c1
khv2644511 <khv2644511@naver.com>
2023-04-06 16:04:35 +0900

    feat: disabled 속성 추가 및 디자인 수정

--- src/components/common/Button/Button.jsx
+++ src/components/common/Button/Button.jsx
@@ -1,5 +1,11 @@
 import { S } from "./ButtonStyle";
 
-export default function Button({ children, theme }) {
-  return <S.Button theme={theme}>{children}</S.Button>;
+export default function Button({ children, bgcolor, disabled, txtcolor }) {
+  console.log(bgcolor);
+
+  return (
+    <S.Button bgcolor={bgcolor} txtcolor={txtcolor} disabled={disabled}>
+      {children}
+    </S.Button>
+  );
 }
--- src/components/common/Button/ButtonStyle.jsx
+++ src/components/common/Button/ButtonStyle.jsx
@@ -1,14 +1,18 @@
 import styled from "styled-components";
 
 const Button = styled.button`
+  background-color: var(${(props) => props.bgcolor});
+  color: var(${(props) => props.txtcolor});
   border: none;
   width: 120px;
   height: 30px;
   border-radius: 5px;
-  background-color: var(--accent-color);
   font-size: 1rem;
-  color: var(--color-type-02);
   cursor: pointer;
+  :disabled {
+    background-color: var(--color-type-03);
+    cursor: default;
+  }
 `;
 
 export const S = { Button };
