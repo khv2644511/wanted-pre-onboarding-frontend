71bfd2a8d1c03acb1ad7392447e2b3bc9f33dc2a
khv2644511 <khv2644511@naver.com>
2023-04-06 11:09:01 +0900

    feat: SignUp 컴포넌트 마크업 및 UI

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,5 +1,23 @@
 import React from "react";
+import Button from "../../components/common/Button/Button";
+import { S } from "./SignupStyle";
 
 export default function SignUp() {
-  return <div>SignUp</div>;
+  return (
+    <main>
+      <h1 className="ir-hidden">로그인페이지</h1>
+      <S.Container>
+        <S.Title>Sign Up</S.Title>
+        <S.FormContainer>
+          <label>Email</label>
+          <input type="email" placeholder="email" width="auto" />
+          <label>Password</label>
+          <input type="password" placeholder="password" />
+          <label>Confirm Password</label>
+          <input type="password" placeholder="password" />
+          <Button theme={""}>Sign In</Button>
+        </S.FormContainer>
+      </S.Container>
+    </main>
+  );
 }
