68db2f6adf757f705cd90ad4f8efe3778c9f576f
khv2644511 <khv2644511@naver.com>
2023-04-08 02:17:52 +0900

    feat: TodoList로 부터 ToItem 컴포넌트 분리 및 생성

--- src/components/todo/TodoList/TodoListStyle.jsx
+++ src/components/todo/TodoList/TodoListStyle.jsx
@@ -3,42 +3,7 @@ import styled from "styled-components";
 const ContainerUl = styled.ul`
   height: 100%;
 `;
-const TodoItemList = styled.li`
-  display: flex;
-  border-bottom: 2px solid var(--color-type-03);
-  margin-bottom: 20px;
-  margin: 0 5px 20px;
-  padding-bottom: 5px;
-  justify-content: space-between;
-  align-items: center;
-  label {
-    display: flex;
-    flex-basis: auto;
-    max-width: 85%;
-  }
-`;
-
-const TodoText = styled.span`
-  width: 300px;
-  font-size: 1rem;
-  text-overflow: ellipsis;
-  overflow: hidden;
-  display: -webkit-box;
-  -webkit-box-orient: vertical;
-  -webkit-line-clamp: 2;
-  margin: 0 10px;
-  line-height: normal;
-  text-align: left;
-`;
-
-const ButtonContainer = styled.div`
-  display: flex;
-  gap: 10px;
-`;
 
 export const S = {
   ContainerUl,
-  TodoItemList,
-  TodoText,
-  ButtonContainer,
 };
