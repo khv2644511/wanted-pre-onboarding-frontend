1a7dccb9870f1d34aa1e30152484b53b67147da2
khv2644511 <khv2644511@naver.com>
2023-04-08 02:19:07 +0900

    feat: Todo 페이지 컴포넌트 UI 생성

--- src/pages/Todo/Todo.jsx
+++ src/pages/Todo/Todo.jsx
@@ -1,5 +1,20 @@
 import React from "react";
+import { S } from "./TodoStyle";
+import LogoutButton from "../../components/todo/LogoutButton/LogoutButton";
+import TodoInput from "../../components/todo/TodoInput/TodoInput";
+import ToDoList from "../../components/todo/TodoList/TodoList";
 
 export default function Todo() {
-  return <div>Todo</div>;
+  return (
+    <S.Container>
+      <S.Header>
+        <S.Title>Todo List</S.Title>
+        <LogoutButton />
+      </S.Header>
+      <S.TodoContainer>
+        <TodoInput />
+        <ToDoList />
+      </S.TodoContainer>
+    </S.Container>
+  );
 }
---
+++ src/pages/Todo/TodoStyle.jsx
@@ -0,0 +1,46 @@
+import styled from "styled-components";
+
+const Container = styled.main`
+  display: flex;
+  flex-direction: column;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 400px;
+  min-height: 300px;
+  background-color: var(--color-type-02);
+  border: 2px solid var(--color-type-03);
+  border-radius: 10px;
+  text-align: center;
+`;
+const Header = styled.header`
+  display: flex;
+  justify-content: space-between;
+  padding-right: 20px;
+  align-items: center;
+`;
+const Title = styled.h2`
+  font-family: "Noto Sans KR", sans-serif;
+  font-size: 2rem;
+  width: 100%;
+  padding: 10px;
+  height: 70px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 10px 10px 0 0;
+  color: var(--color-type-01);
+`;
+
+const TodoContainer = styled.section`
+  width: 100%;
+  height: 100%;
+`;
+
+export const S = {
+  Container,
+  Header,
+  Title,
+  TodoContainer,
+};
