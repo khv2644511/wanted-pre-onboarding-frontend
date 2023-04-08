1508bd9da81a68f4be821ae76968901783985dc7
khv2644511 <khv2644511@naver.com>
2023-04-08 16:17:01 +0900

    feat: Todo 조회 기능 추가

--- src/components/todo/TodoList/TodoList.jsx
+++ src/components/todo/TodoList/TodoList.jsx
@@ -1,12 +1,16 @@
 import TodoItem from "../TodoItem/TodoItem";
 import { S } from "./TodoListStyle";
 
-export default function ToDoList() {
+export default function ToDoList({ todos, isLoading }) {
   return (
     <S.ContainerUl>
-      <TodoItem />
-      <TodoItem />
-      <TodoItem />
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : todos ? (
+        todos.map((todo) => <TodoItem key={todo.id} todo={todo.todo} />)
+      ) : (
+        <p>Todo를 작성해보세요!</p>
+      )}
     </S.ContainerUl>
   );
 }
