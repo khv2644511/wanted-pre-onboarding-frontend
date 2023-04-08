48d5ca46a7481e234b32d078705191d651c0bdf5
khv2644511 <khv2644511@naver.com>
2023-04-09 05:29:27 +0900

    feat: Router 컴포넌트 분리로 인한 수정

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
-function App() {
-  return <>App</>;
-}
+import React from "react";
+import GlobalStyle from "./components/style/GlobalStyled";
+import Router from "./router/Router";
 
-export default App;
+export default function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <Router />
+    </>
+  );
+}
