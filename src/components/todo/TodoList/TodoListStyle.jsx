66b46b737699f78dfcbccf9edfcd37c83406ec3d
khv2644511 <khv2644511@naver.com>
2023-04-08 16:20:22 +0900

    design: TodoList 스크롤 추가

--- src/components/todo/TodoList/TodoListStyle.jsx
+++ src/components/todo/TodoList/TodoListStyle.jsx
@@ -2,6 +2,11 @@ import styled from "styled-components";
 
 const ContainerUl = styled.ul`
   height: 100%;
+  max-height: 250px;
+  overflow-y: auto;
+  p {
+    font-size: 2rem;
+  }
 `;
 
 export const S = {
