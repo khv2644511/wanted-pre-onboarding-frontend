bafae0c09a23e5fb22d50e7c40b3872df26c7abd
khv2644511 <khv2644511@naver.com>
2023-04-08 21:31:06 +0900

    feat: 수정버튼 클릭 시 수정입력창 조건부 렌더링 및 style 수정

--- src/components/todo/TodoItem/TodoItem.jsx
+++ src/components/todo/TodoItem/TodoItem.jsx
@@ -1,22 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import { S } from "./TodoItemStyle";
 import ModifyButton from "../ModifyButton/ModifyButton";
 import RemoveButton from "../RemoveButton/RemoveButton";
 import CheckBox from "../CheckBox/CheckBox";
+import CancelButton from "../CancelButton/CancelButton";
+import SubmitButton from "../SubmitButton/SubmitButton";
 
 export default function TodoItem({ todoData, getTodos }) {
   const { todo, id } = todoData;
+  const [modifyClick, setModifyClick] = useState(true);
 
   return (
     <S.TodoItemList>
-      <label>
-        <CheckBox todoData={todoData} />
-        <S.TodoText>{todo}</S.TodoText>
-      </label>
-      <S.ButtonContainer>
-        <ModifyButton />
-        <RemoveButton todoId={id} getTodos={getTodos} />
-      </S.ButtonContainer>
+      {modifyClick ? (
+        <S.Form>
+          <CheckBox todoData={todoData} />
+          <S.ModifyInput type="text" />
+          <SubmitButton />
+          <CancelButton />
+        </S.Form>
+      ) : (
+        <>
+          <label>
+            <CheckBox todoData={todoData} />
+            <S.TodoText>{todo}</S.TodoText>
+          </label>
+          <S.ButtonContainer>
+            <ModifyButton
+              onclick={() => {
+                setModifyClick((prev) => !prev);
+              }}
+            />
+            <RemoveButton todoId={id} getTodos={getTodos} />
+          </S.ButtonContainer>{" "}
+        </>
+      )}
     </S.TodoItemList>
   );
 }
