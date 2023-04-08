dfd30f83d98187d6ab3009eb33e6567669c5a6fb
khv2644511 <khv2644511@naver.com>
2023-04-08 17:48:31 +0900

    feat: props 수정 및 getTodos props 추가

--- src/components/todo/TodoList/TodoList.jsx
+++ src/components/todo/TodoList/TodoList.jsx
@@ -1,13 +1,19 @@
 import TodoItem from "../TodoItem/TodoItem";
 import { S } from "./TodoListStyle";
 
-export default function ToDoList({ todos, isLoading }) {
+export default function ToDoList({ todos, isLoading, getTodos }) {
   return (
     <S.ContainerUl>
-      {isLoading ? (
-        <p>Loading...</p>
-      ) : todos.length !== 0 ? (
-        todos.map((todo) => <TodoItem key={todo.id} todo={todo.todo} />)
+      {isLoading ? ( //
+        <p>Loading...</p> //
+      ) : todos.length !== 0 ? ( //
+        todos.map((todo) => (
+          <TodoItem
+            key={todo.id} //
+            todoData={todo} //
+            getTodos={getTodos}
+          />
+        ))
       ) : (
         <p>Todo를 작성해보세요!</p>
       )}
