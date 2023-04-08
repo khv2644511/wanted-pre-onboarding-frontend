53683d8823a6f48ea756ce896852acf2a3c96c73
HYEBIN KIM <khv2644511@naver.com>
2023-04-09 02:29:12 +0900

    feat: 로그아웃 기능 추가 (#28)

--- src/components/todo/LogoutButton/LogoutButton.jsx
+++ src/components/todo/LogoutButton/LogoutButton.jsx
@@ -1,9 +1,15 @@
+import { useCallback } from "react";
 import { FiLogOut } from "react-icons/fi";
 import { S } from "./LogoutButtonStyle";
 
 export default function LogoutButton({ onClick }) {
+  const handleLogout = useCallback(() => {
+    localStorage.removeItem("access_token");
+    window.location.href = "/signin";
+  }, []);
+
   return (
-    <S.Button onClick={onClick}>
+    <S.Button onClick={handleLogout}>
       <FiLogOut />
     </S.Button>
   );
