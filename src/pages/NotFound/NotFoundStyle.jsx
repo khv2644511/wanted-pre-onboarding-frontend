d99bfd84e6a474e6ed905df7fdc2d7fb098e046f
HYEBIN KIM <khv2644511@naver.com>
2023-04-09 05:35:51 +0900

    Feat: Router 컴포넌트 생성 및 수정 (#34)
    * remove: 사용되지 않는 페이지 삭제
    
    * feat: Router 컴포넌트 분리
    
    * feat: Router 컴포넌트 분리로 인한 수정
    
    * desgin: NotFound 페이지 UI

---
+++ src/pages/NotFound/NotFoundStyle.jsx
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+const Container = styled.article`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 400px;
+  height: 320px;
+  background-color: var(--color-type-02);
+  border: 2px solid var(--color-type-03);
+  border-radius: 10px;
+  text-align: center;
+`;
+
+const Title = styled.h2`
+  font-family: "Noto Sans KR", sans-serif;
+  font-size: 2rem;
+  width: 100%;
+  padding: 10px;
+  height: 70px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 10px 10px 0 0;
+  color: var(--color-type-01);
+`;
+
+export const S = { Container, Title };
