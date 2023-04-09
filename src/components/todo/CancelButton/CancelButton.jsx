225ee848b02fae4493c246c7349a685d03aed51a
khv2644511 <khv2644511@naver.com>
2023-04-09 14:36:10 +0900

    fix: Form submission canceled because the form is not connected Warning 해결

--- src/components/todo/CancelButton/CancelButton.jsx
+++ src/components/todo/CancelButton/CancelButton.jsx
@@ -4,7 +4,7 @@ import { S } from "./CancelButtonStyle";
 
 export default function CancelButton({ onclick }) {
   return (
-    <S.Button onClick={onclick} data-testid="cancel-button">
+    <S.Button onClick={onclick} data-testid="cancel-button" type="button">
       <ImCancelCircle />
     </S.Button>
   );
