f90b1d5cd5bef07001e5df0d5bfadc1bbe04da79
khv2644511 <khv2644511@naver.com>
2023-04-09 14:43:20 +0900

    fix: 로그아웃 시 에러 문제 해결 및 TodoInput 에 getTodos props로 전달하여 todo 생성 시 재렌더링 되지 않는 문제 해결

--- src/components/todo/LogoutButton/LogoutButton.jsx
+++ src/components/todo/LogoutButton/LogoutButton.jsx
@@ -1,12 +1,11 @@
-import { useCallback } from "react";
 import { FiLogOut } from "react-icons/fi";
 import { S } from "./LogoutButtonStyle";
 
 export default function LogoutButton({ onClick }) {
-  const handleLogout = useCallback(() => {
+  const handleLogout = () => {
     localStorage.removeItem("access_token");
     window.location.href = "/signin";
-  }, []);
+  };
 
   return (
     <S.Button onClick={handleLogout}>
